b8a1b1df85fb6de014576b7c72fbf5efdac3bbd7
gotamey <ipg.spg@gmail.com>
2023-05-01 18:20:30 +0530

    pipelines test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
                     <div class="hidden lg:flex lg:gap-x-12">
                         <div x-data="{dropdownMenu: false}" class="relative">
                             <button @click="dropdownMenu = ! dropdownMenu" type="button" class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
-                                Product    
+                                Product Details  
                                 <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                                 </svg>
